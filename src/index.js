b03705b583b1bc87e4b961644e66ac0b729aef87
Akash <subs@appsparkler.com>
2020-01-12 12:11:37 +0530

    WIP

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import 'bootstrap'
+import 'bootstrap/scss/bootstrap.scss'
+import "./scss/style.sass"
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,27 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
     mode: 'production',
     entry: './src/index.js',
     output: {
-      path: __dirname + '/js',
+      path: __dirname + '/assets',
       filename: '[name].[contenthash:8].js',
-      publicPath: '/assets/js'
+      publicPath: '/assets/'
+    },
+    module: {
+      rules:[
+        {
+          test: /\.s[ac]ss$/,
+          loaders: [
+            'style-loader',
+            { loader:MiniCssExtractPlugin.loader, options: { hmr: false } },
+            'css-loader',
+            'sass-loader'
+          ]
+        }
+      ],
     },
     plugins: [
       new CleanWebpackPlugin({
@@ -15,7 +30,13 @@ module.exports = {
       }),
       new HtmlWebpackPlugin({
         template: './src/index.ejs',
-        filename: '../../_includes/webpacked-libs.html'
+        filename: '../_includes/webpacked-libs.html'
+      }),
+       new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: '[name].css',
+        chunkFilename: '[id].css',
       })
     ]
 }
